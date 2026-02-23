9af446a51538cc4c79c62e600e905c2a387c2559
ayu-syaacc <ayusyafnizuriyanti@gmail.com>
2026-02-23 20:10:33 +0700

    ini tugas

--- pertemuan3/Inheritance.py
+++ pertemuan3/Inheritance.py
@@ -1,4 +1,60 @@
-class animal:
- def __init__ (self, name):
-    self.name = name
+class Person:
+  def __init__(self, fname, lname):
+    self.firstname = fname
+    self.lastname = lname
 
+  def printname(self):
+    print(self.firstname, self.lastname)
+
+#Use the Person class to create an object, and then execute the printname method:
+
+x = Person("John", "Doe")
+x.printname()
+
+#Create a Child Class
+class Student(Person):
+  def __init__(self, fname, lname, year):
+    Person.__init__(self, fname, lname)
+    self.graduationyear = year
+
+  def printname(self):
+    print(self.firstname, self.lastname, self.graduationyear)
+
+  def printname(self):
+    print(self.firstname, self.lastname)
+
+class Student(Person):
+  pass
+
+x = Student("Mike", "Olsen")
+x.printname()
+
+#__init__()
+class Student(Person):
+  def __init__(self, fname, lname):
+    #add properties etc.
+
+#super() function
+    super().__init__(fname, lname)
+
+#add properties 
+class Student(Person):
+  def __init__(self, fname, lname):
+    super().__init__(fname, lname)
+    self.graduationyear = 2019
+
+class Student(Person):
+  def __init__(self, fname, lname, year):
+    super().__init__(fname, lname)
+    self.graduationyear = year
+
+x = Student("Mike", "Olsen", 2019)
+
+#add methods
+class Student(Person):
+  def __init__(self, fname, lname, year):
+    super().__init__(fname, lname)
+    self.graduationyear = year
+
+  def welcome(self):
+    print("Welcome", self.firstname, self.lastname, "to the class of", self.graduationyear)
